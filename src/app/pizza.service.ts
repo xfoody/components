faa53747b9cd175c42307025d21e85e202e41a6c
Sonam Dolker Kindy <47401455+skindy-matc@users.noreply.github.com>
2019-03-04 19:53:21 -0600

    Using the pizza service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  public loadPizzaToppings() {
+    // This is a pretend const that will ultimately 
+    // come from a web service call (coming soon).
+    const pizzaToppingsFromWebService = [
+      { name: 'Pepperoni', price: 1.50 }
+      , { name: 'Olives', price: 3.50 }
+    ];
+
+    return pizzaToppingsFromWebService.map(x => ({
+      ...x
+      , checked: false
+    }));
+  }
 }
