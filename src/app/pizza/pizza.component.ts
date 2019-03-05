df854e8aa94ad37c613dac0849b40e5b5627b5ce
wgeorgeson <43157121+wgeorgeson@users.noreply.github.com>
2019-03-04 19:53:42 -0600

    Using the pizza service.....yay!

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza',
   templateUrl: './pizza.component.html',
   styleUrls: ['./pizza.component.css']
 })
+
 export class PizzaComponent implements OnInit {
 
-  constructor() { }
+  // Magic Dependency Injection!!!
+  constructor(public pizzaSvc: PizzaService) { }
 
   ngOnInit() {
+    // Use the pizza service to load the available pizza toppings.
+    // let svc = new PizzaService();
+    // Use DI instead...
+
+    const foo = this.pizzaSvc.loadPizzaToppings();
+    console.log(foo);
   }
 
 }
