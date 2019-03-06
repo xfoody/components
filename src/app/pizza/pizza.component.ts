2b5384bff1d29cbddea96b27e14c87074f55c916
TanwiSharma <tsharma@madisoncollege.edu>
2019-03-05 21:41:29 -0600

    All working!!
    1> Added button to calculate total price.
    2> Used FormsMudule import statement:-
    
    import { FormsModule } from '@angular/forms';

--- src/app/pizza/pizza.component.ts
+++ src/app/pizza/pizza.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {PizzaService} from '../pizza.service';
 
+interface PizzaToppingDisplay {
+    name: string;
+    price: number;
+    checked: boolean;
+  }
 @Component({
   selector: 'app-pizza',
   templateUrl: './pizza.component.html',
   styleUrls: ['./pizza.component.css']
 })
 export class PizzaComponent implements OnInit {
-    public availablePizzaToppings;
+    public availablePizzaToppings: PizzaToppingDisplay[];
+    total = 0;
+    public calculateTotal = () => {
+        this.total = this.availablePizzaToppings
+          .filter(x => x.checked)
+          .reduce(
+            (acc, x) => acc + x.price
+            , 0
+          );
+      };
 //magic DI
   constructor(public pizzaSvc: PizzaService) { }
 
