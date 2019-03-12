199e1deae443dc56b33c543289162071fc052c96
Sonam Dolker Kindy <47401455+skindy-matc@users.noreply.github.com>
2019-03-11 20:23:22 -0500

    Added more material components to pizza component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
+import { MatCardModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,6 +17,8 @@ import { MatCardModule } from '@angular/material/card';
     , FormsModule
     , BrowserAnimationsModule
     , MatCardModule
+    , MatButtonModule
+    , MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
