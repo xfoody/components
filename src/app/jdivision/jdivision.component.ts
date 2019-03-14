29cecc2ebe9bde0c09c0e1c0881eb50b04d3305c
udarc <46758935+udarc@users.noreply.github.com>
2019-03-14 12:51:47 -0500

    Completed division functionalities

--- src/app/jdivision/jdivision.component.ts
+++ src/app/jdivision/jdivision.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JdivisionComponent implements OnInit {
 
+  dividend = 14;
+  divider = 4;
+  quotient = 0;
+  show: boolean = true;
+  errorMessage = "";
+
+  divideNumbers =  () =>{
+    if(this.divider !== 0) {
+      this.quotient = this.dividend/ this.divider;
+      this.show = true;
+      this.errorMessage = ""
+    } else {
+      this.errorMessage = "You can't divide a number with zero";
+      this.show = false;
+
+      return;
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
