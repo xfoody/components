6ea3036286db29e821cd356790b80adffc849aed
zpcoffey <43156754+zpcoffey@users.noreply.github.com>
2019-03-26 08:57:29 -0500

    generated component
    first step

---
+++ src/app/zpc-subtract/zpc-subtract.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zpc-subtract',
+  templateUrl: './zpc-subtract.component.html',
+  styleUrls: ['./zpc-subtract.component.css']
+})
+export class ZpcSubtractComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
